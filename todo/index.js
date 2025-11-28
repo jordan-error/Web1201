b090bc1068871da17464b3f1244ddcbd27cd3e02
Draterr <benpooncc@outlook.com>
2025-11-29 01:10:17 +0800

    refactored javascript code

--- todo/index.js
+++ todo/index.js
@@ -1,18 +1,28 @@
 var plus_symbol = document.getElementById("plus_symbol");
 var notes_container = document.getElementById("notes_container");
-var current_list = "1";
-var current_notes = JSON.parse(localStorage.getItem(current_list));
-var number_of_lists = 0;
+var current_list_id = "35fd7e4c-5e49-4b36-8d81-f9da7b66bc82";
+var all_lists = JSON.parse(localStorage.getItem("todolist"));
+var note_map = {};
+var list_map = {};
+var current_list = {}; 
+var current_notes = {};
 
-
+//Load the lists and notes into memory so i can do constant time lookup with dictionaries instead of having to iterate over arr_lists everytime
+function load_map(){
+	for(const list of all_lists){
+		list_map[list.id] = list;
+		note_map[list.id] = list.notes;
+	}
+}
 function update_current_list(list_id){
-	current_list = list_id;
+	current_list_id = list_id;
+	console.log(`Switched to list ${current_list_id}`);
 	//remove the selected class from previous selected list item
 	for (const i of document.querySelectorAll(".selected")){
 		i.classList.remove("selected");
 	}
 	//give the list item the selected class 
-	document.getElementById(`list${current_list}_nav_title`).classList.add("selected");
+	document.getElementById(`${current_list_id}_nav_title`).classList.add("selected");
 	clear_canvas();
 	init_list();
 }
@@ -24,49 +34,82 @@ function clear_canvas(){
 	}
 }
 
+function update_localstorage(){
+	localStorage.setItem("todolist",JSON.stringify(all_lists));
+}
+
 function init_list(){
-	if(!localStorage.getItem(current_list)){
-		localStorage.setItem(current_list,'{"title":"LIST"}');
+	if(!localStorage.getItem("todolist")){
+		localStorage.setItem("todolist",JSON.stringify([{"id":"35fd7e4c-5e49-4b36-8d81-f9da7b66bc82","title":"LIST","timestamp":new Date(),"notes":{}}]))
+		all_lists = JSON.parse(localStorage.getItem("todolist"));
 	}
-	current_notes = JSON.parse(localStorage.getItem(current_list));
-	document.getElementById("title").value = current_notes["title"];
+	load_map();
+	if(!list_map[current_list_id]){
+		new_list = {"id":current_list_id,"title":"LIST","timestamp":new Date(),"notes":{}};
+		all_lists.push(new_list);
+		list_map[current_list_id] = new_list;
+		note_map[current_list_id] = {};
+		update_localstorage();
+	}
+	current_list = list_map[current_list_id];
+	current_notes = note_map[current_list_id];
+	document.getElementById("title").value = current_list["title"];
+	console.log(current_notes)
 	load_notes();
 }
 
+function load_notes(){
+	for(const notes in current_notes){
+		create_span({parentNode:notes_container,id:notes,old_text:current_notes[notes].content});
+	}
+}
+
 function init_nav(){
-	for(i=1;i<=localStorage.length;i++){
-		if(i !== 1){
-			let current_context = JSON.parse(localStorage.getItem(i));
+	for(const i of Object.keys(list_map)){
+		let current_context = list_map[i];
+		if(i != "35fd7e4c-5e49-4b36-8d81-f9da7b66bc82"){
 			create_new_nav_option(i);
-			document.getElementById(`list${i}_nav_title`).textContent = current_context["title"];
-			document.getElementById(`${i}_num`).innerText = Object.keys(current_context).length - 1;
+			document.getElementById(`${i}_num`).innerText = Object.keys(current_context.notes).length;
 		}
+		document.getElementById(`${i}_nav_title`).textContent = current_context["title"];
 	}
 }
 
 
-function update_localstorage(list_id,dict){
-	localStorage.setItem(list_id,JSON.stringify(dict));
+function add_note(note_dict){
+	current_list.notes[note_dict.note_id] = note_dict;
+	update_localstorage();
+}
+function remove_note(note_id){
+	delete current_notes[note_id];
+	update_localstorage();
+}
+function remove_list(list_id){
+	let i = 0;
+	for(const list of all_lists){
+		if(list.id === list_id){
+			all_lists.splice(i,1);
+		}
+		i++;
+	}
 }
 function create_span({parentNode,id=null,old_text=null}={}){
 	function create_whole_span(new_div){
 		new_div.classList.add("notes");
 		create_garbage(new_div);
-		// new_div.onclick = function(event){
-		// 	buttons_interact(event,this)
-		// }
 	}
 	let new_span = document.createElement('div');
 	id = id ? id : crypto.randomUUID();
 	parentNode.insertBefore(new_span,plus_symbol);
 	if(old_text === null){
-		current_notes[id] = "";
-		update_localstorage(current_list,current_notes);
+		add_note({"note_id":id,"header":"","content":""});
+		console.log(`Note Added to list_id: ${current_list} note_id: ${id}`);
 	}
 	create_textbox({parentNode:new_span,old_text:old_text,rec_id:id});
 	new_span.setAttribute('rec_id',id);
 	create_whole_span(new_span,'not-started');
 	update_notes_count();
+	update_localstorage();
 	return new_span;
 }
 
@@ -94,10 +137,10 @@ function create_garbage(parentNode){
 	div.addEventListener("click",()=>{
 		id = parentNode.getAttribute('rec_id');
 		parentNode.remove();
-		delete current_notes[id];
-		update_localstorage(current_list,current_notes);
+		remove_note(id);
 		update_notes_count();
-		number_of_lists = document.getElementsByTagName("li").length-1;
+		update_localstorage();
+		console.log(`Note Removed: ${id}`);
 	})
 }
 
@@ -118,11 +161,14 @@ function create_x(parentNode) {
     path.setAttribute('fill', 'rgb(225 94 106)'); 
     path.setAttribute("d","M183.1 137.4C170.6 124.9 150.3 124.9 137.8 137.4C125.3 149.9 125.3 170.2 137.8 182.7L275.2 320L137.9 457.4C125.4 469.9 125.4 490.2 137.9 502.7C150.4 515.2 170.7 515.2 183.2 502.7L320.5 365.3L457.9 502.6C470.4 515.1 490.7 515.1 503.2 502.6C515.7 490.1 515.7 469.8 503.2 457.3L365.8 320L503.1 182.6C515.6 170.1 515.6 149.8 503.1 137.3C490.6 124.8 470.3 124.8 457.8 137.3L320.5 274.7L183.1 137.4z");
     svg.appendChild(path);
-	div.addEventListener("click",()=>{
-		let delete_list_num = parentNode.getAttribute("list");
-		console.log(delete_list_num);
-		localStorage.removeItem(delete_list_num)
+	div.addEventListener("click",(event)=>{
+		event.preventDefault();
+		let delete_list_id = parentNode.getAttribute("list");
+		delete list_map[delete_list_id];
+		remove_list(delete_list_id);
 		parentNode.remove();
+		update_localstorage();
+		console.log(`removed list: ${delete_list_id}`);
 	})
 }
 
@@ -134,58 +180,52 @@ function create_textbox({parentNode,old_text,rec_id} ={}){
 	if(old_text)text_box.value = old_text;
 	parentNode.appendChild(text_box);
 	text_box.addEventListener("input",()=>{
-		current_notes[rec_id] = text_box.value;
-		update_localstorage(current_list,current_notes);
+		current_notes[rec_id].content = text_box.value;
+		update_localstorage();
 	})
 }
 
-function load_notes(){
-	for(const notes in current_notes){
-		if(notes !== "title"){
-			create_span({parentNode:notes_container,id:notes,old_text:current_notes[notes]});
-		}
-	}
-}
 
 function update_notes_count(){
 	//Minus 1 becasue of "title" key
-	document.getElementById(`${current_list}_num`).innerText = Object.keys(current_notes).length - 1;
+	let len = Object.keys(current_notes).length;
+	document.getElementById(`${current_list_id}_num`).innerText = len;
+	console.log(`note count has changed to ${len}`)
 }
 
 
-function create_new_nav_option(number=null){
+function create_new_nav_option(uuid=null){
 	let current_a = document.createElement("a");
-	let current_num_lists = number ? number : ++number_of_lists;
-	current_a.setAttribute("list",current_num_lists);
-	current_a.href = `javascript:update_current_list('${current_num_lists}')`;
+	let new_note_uuid = uuid ? uuid : crypto.randomUUID();
+	current_a.setAttribute("list",new_note_uuid);
+	current_a.href = `javascript:update_current_list('${new_note_uuid}')`;
 	current_a.classList.add("list-item");
 	let current_li = document.createElement("li");
-	current_li.id = `list${current_num_lists}_nav_title`;
+	current_li.id = `${new_note_uuid}_nav_title`;
 	current_li.textContent = "LIST";
 	let current_p = document.createElement("p");
 	current_p.classList.add("tasks-num");
-	current_p.id = `${current_num_lists}_num`
+	current_p.id = `${new_note_uuid}_num`
 	current_p.textContent = 0;
 	current_a.appendChild(current_li);
 	current_a.appendChild(current_p);
 	let navbar = document.getElementById("left-navbar");
 	navbar.insertBefore(current_a,document.getElementById("add-list-item"));
-	update_current_list(`${current_num_lists}`)
-	number_of_lists = document.getElementsByTagName("li").length-1;
+	update_current_list(`${new_note_uuid}`)
 	create_x(current_a)
+	console.log(`New List Created: ${new_note_uuid}`);
 }
 
 plus_symbol.addEventListener("click",()=>{ 
 	 create_span({parentNode:notes_container});
 })
 document.getElementById("title").addEventListener("input",()=>{
-	current_notes["title"] = document.getElementById("title").value;
-	document.getElementById(`list${current_list}_nav_title`).textContent = current_notes['title'];
-	localStorage.setItem(current_list,JSON.stringify(current_notes));
+	current_list["title"] = document.getElementById("title").value;
+	document.getElementById(`${current_list_id}_nav_title`).textContent = current_list['title'];
+	update_localstorage();
 })
 
 document.addEventListener("DOMContentLoaded",()=>{
 	init_list();
 	init_nav();
-	number_of_lists = document.getElementsByTagName("li").length-1;
 })
